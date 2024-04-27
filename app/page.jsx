8d4c4c3a97b935134a6805562441ba804b428ee2
dineshkush <kushwahd18@gmail.com>
2024-04-27 22:44:36 +0530

    prompt assist

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,15 @@
+'use client';
+
 import Feed from "@components/Feed";
+import { useState } from "react";
 
 const Home = () => {
+  const [feedPosts, setFeedPosts] = useState([]);
+
+  const updateFeed = (updatedPosts) => {
+    setFeedPosts(updatedPosts);
+  };
+
   return (
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
@@ -11,7 +20,7 @@ const Home = () => {
         <p className="desc text-center">
             Promptassist is an open-source AI prompting tool for modern world to discover, create and share creative prompts
         </p>
-        <Feed />
+        <Feed updateFeed={updateFeed} />
     </section>
   )
 }
